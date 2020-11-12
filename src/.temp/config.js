e531180b7997a793ae50d22fc43ac8df69cd392f
Kim Jiho <go2zo.ooo@gmail.com>
2020-11-12 20:41:23 +0900

    [#7] 자동생성 디렉토리 git 삭제
     - src/.temp 삭제
    
     - .gitignore에 'src/.temp/' 추가
    
    Resolves #7

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Go2zo",
-  "siteUrl": "https://go2zo.github.io",
-  "version": "0.7.22",
-  "catchLinks": true
-}
